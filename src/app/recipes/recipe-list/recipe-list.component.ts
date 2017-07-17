ffe21e966669ffb1b02f760517152acc52351541
Amazetbm <jtalley1@gmail.com>
2017-07-17 13:06:14 -0400

    Added shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,16 +7,21 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test',
       'http://images.media-allrecipes.com/userphotos/250x250/02/57/31/2573174.jpg'),
-    new Recipe('A Test Recipe', 'This is a test',
+    new Recipe('Another Test Recipe', 'This is another test',
       'http://images.media-allrecipes.com/userphotos/250x250/02/57/31/2573174.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
